ffe837be055dd95b024bec6e48394f65392203d5
Meena898 <aneeeeem.1311@gmail.com>
2025-01-10 05:49:57 +0000

    any name about project

---
+++ day1.py
@@ -0,0 +1,10 @@
+ 
+integer_var = 10               
+float_var = 3.14               
+string_var = "Hello, world!"   
+boolean_var = True           
+ 
+print(f"Value: {integer_var}, Type: {type(integer_var)}")
+print(f"Value: {float_var}, Type: {type(float_var)}")
+print(f"Value: {string_var}, Type: {type(string_var)}")
+print(f"Value: {boolean_var}, Type: {type(boolean_var)}")
