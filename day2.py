566a73b20538129094d872dfcf3c43c152871138
Meena898 <aneeeeem.1311@gmail.com>
2025-01-11 15:07:53 +0000

    any name about project

---
+++ day2.py
@@ -0,0 +1,13 @@
+my_list = [10, 20, 30, 40, 50]
+my_tuple = (100, 200, 300, 400, 500)
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York',
+    'job': 'Engineer',
+    'hobby': 'Reading'
+}
+
+print(my_list[0], my_list[2], my_list[-1], my_list[-2])
+print(my_tuple[1], my_tuple[3], my_tuple[0], my_tuple[-1])
+print(my_dict['name'], my_dict['age'], my_dict['job'])
